c0a502db95fb7c955d4c2cbf5e22d18fc56a27d7
johnkirtley <kirtleyj16@gmail.com>
2020-03-19 09:06:21 -0700

    Fixed step number query

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -22,7 +22,9 @@ function findById(id) {
 function findSteps(id) {
 	return db('steps')
 		.join('schemes', 'schemes.id', 'steps.scheme_id')
-		.select('schemes.scheme_name', 'steps.step_number', 'steps.instructions');
+		.select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+		.where('schemes.id', id)
+		.orderBy('steps.step_number');
 }
 
 function add(scheme) {
